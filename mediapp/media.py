4f60fe196c28cdfb945d62428093e680cfdffe41
marcos <m_german4@mail.fhsu.edu>
2024-10-27 17:44:03 -0500

    add option to delete and edit

--- mediapp/media.py
+++ mediapp/media.py
@@ -16,9 +16,8 @@ def index():
 def movies():
     db = get_db()
     movies = db.execute(
-        'SELECT title, rating, review, added_on'
-        ' FROM movies WHERE user_id = ?'
-        ' ORDER BY rating DESC', (g.user['id'],)
+        'SELECT id, title, rating, review FROM movies WHERE user_id = ?',
+        (g.user['id'],)
     ).fetchall()
     return render_template('media/movies.html', movies=movies)
 
@@ -27,7 +26,7 @@ def movies():
 def series():
     db = get_db()
     series = db.execute(
-        'SELECT title, rating, review, season, episode, status, added_on'
+        'SELECT id, title, rating, review, season, episode, status, added_on'
         ' FROM series WHERE user_id = ?'
         ' ORDER BY rating DESC', (g.user['id'],)
     ).fetchall()
@@ -89,4 +88,86 @@ def toggle_status(id):
         (new_status, id, g.user['id'])
     )
     db.commit()
+    return redirect(url_for('media.series'))
+
+
+# Edit Movie Route
+@bp.route('/edit_movie/<int:id>', methods=('GET', 'POST'))
+@login_required
+def edit_movie(id):
+    db = get_db()
+    movie = db.execute(
+        'SELECT * FROM movies WHERE id = ? AND user_id = ?',
+        (id, g.user['id'])
+    ).fetchone()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        rating = request.form['rating']
+        review = request.form.get('review')
+
+        db.execute(
+            'UPDATE movies SET title = ?, rating = ?, review = ? WHERE id = ? AND user_id = ?',
+            (title, rating, review, id, g.user['id'])
+        )
+        db.commit()
+        flash('The movie has been updated successfully!', 'success')
+        return redirect(url_for('media.movies'))
+
+    return render_template('media/edit_movie.html', movie=movie)
+
+
+# Delete Movie Route
+@bp.route('/delete_movie/<int:id>', methods=('POST',))
+@login_required
+def delete_movie(id):
+    db = get_db()
+    db.execute(
+        'DELETE FROM movies WHERE id = ? AND user_id = ?',
+        (id, g.user['id'])
+    )
+    db.commit()
+    flash('The movie has been deleted successfully!', 'success')
+    return redirect(url_for('media.movies'))
+
+
+# Edit Series Route
+@bp.route('/edit_series/<int:id>', methods=('GET', 'POST'))
+@login_required
+def edit_series(id):
+    db = get_db()
+    serie = db.execute(
+        'SELECT * FROM series WHERE id = ? AND user_id = ?',
+        (id, g.user['id'])
+    ).fetchone()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        rating = request.form['rating']
+        review = request.form.get('review')
+        episode = request.form.get('episode', 1)
+        status = request.form.get('status', 'watching')
+
+        db.execute(
+            'UPDATE series SET title = ?, rating = ?, review = ?, episode = ?, status = ? WHERE id = ? AND user_id = ?',
+            (title, rating, review, episode, status, id, g.user['id'])
+        )
+        db.commit()
+        flash('The TV show has been updated successfully!', 'success')
+        return redirect(url_for('media.series'))
+
+    return render_template('media/edit_series.html', serie=serie)
+
+
+# Delete Series Route
+@bp.route('/delete_series/<int:id>', methods=('POST',))
+@login_required
+def delete_series(id):
+    db = get_db()
+    db.execute(
+        'DELETE FROM series WHERE id = ? AND user_id = ?',
+        (id, g.user['id'])
+    )
+    db.commit()
+    flash('The TV show has been deleted successfully!', 'success')
     return redirect(url_for('media.series'))
